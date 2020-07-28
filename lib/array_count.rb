d96962a18cd8ac157070d6e6d47944ab3f0bdb3c
Alex Foreman <foreman.alexander@gmail.com>
2020-07-28 18:03:03 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,43 @@
 def count_strings(array)
-  array.count do |element|
-    element.class == String 
+  array.count do |strings|
+  strings == 
   end
 end
 
-def count_empty_strings(array)
- array.count do |element|
-  element == ""
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def count_strings(array)
+#   array.count do |element|
+#     element.class == String 
+#   end
+# end
+
+# def count_empty_strings(array)
+# array.count do |element|
+#   element == ""
+#   end
+# end
